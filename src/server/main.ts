e04fd5d194c5731c8042e261ac6c3ab21a2bebff
kimserey lam <kimserey.lam@gmail.com>
2019-07-19 09:49:50 +0100

    pages prisitine node

--- src/server/main.ts
+++ src/server/main.ts
@@ -7,8 +7,22 @@ import {
 const port = process.env.PORT || 3000;
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  res.writeHead(200, { "Content-Type": "text.plain" });
-  res.end("Hello World!");
+  const path = req.url!.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+
+  switch (path) {
+    case "":
+      res.writeHead(200, { "Content-Type": "text.plain" });
+      res.end("Homepage");
+      break;
+    case "/about":
+      res.writeHead(200, { "Content-Type": "text.plain" });
+      res.end("About");
+      break;
+    default:
+      res.writeHead(404, { "Content-Type": "text.plain" });
+      res.end("Not Found");
+      break;
+  }
 });
 
 server.listen(port, () => {
