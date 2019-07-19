36470889253058b0309240b7f323369b00dc3cf8
kimserey lam <kimserey.lam@gmail.com>
2019-07-19 10:27:38 +0100

    Introduction of express

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,42 +1,22 @@
-import { readFile } from "fs";
-import { createServer, IncomingMessage, ServerResponse } from "http";
+import * as e from "express";
 
-type ContentType = "text/html" | "image/png";
-
+const app = e();
 const port = process.env.PORT || 3000;
 
-function serveStaticFile(res: ServerResponse, path: string, contentType: ContentType, responseCode = 200) {
-  readFile(__dirname + path, (err, data: Buffer) => {
-    if (err) {
-      res.writeHead(500, { "Content-Type": "text.plain" });
-      return res.end("Internal Server Error");
-    }
-
-    res.writeHead(responseCode, { "Content-Type": contentType });
-    res.end(data);
-  });
-}
-
-const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  const path = req.url!.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
-
-  switch (path) {
-    case "":
-      serveStaticFile(res, "/public/home.html", "text/html");
-      break;
-    case "/about":
-      serveStaticFile(res, "/public/about.html", "text/html");
-      break;
-    case "/img/logo.png":
-      serveStaticFile(res, "/public/img/logo.png", "image/png");
-      break;
-    default:
-      serveStaticFile(res, "/public/404.html", "text/html", 404);
-      break;
-  }
+app.use((_: e.Request, res: e.Response) => {
+  res.type("text/plain");
+  res.status(404);
+  res.send("Not Found");
 });
 
-server.listen(port, () => {
-  console.log(`Server started on port ${port}.`);
-  console.log("press Ctrl-C to terminate....");
+app.use((err: any, req: e.Request, res: e.Response, next: e.NextFunction) => {
+  console.error(err.message);
+  res.type("text/plain");
+  res.status(500);
+  res.send("Server Error");
+});
+
+app.listen(port, () => {
+  console.log(`Express started on http://localhost:${port}`);
+  console.log("Press CTRL+C to terminate.");
 });
